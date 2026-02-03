77cbd06be51cd7fe67869af0e7f9ca30a297c43b
Abdur Mohammed <abdur@sven.global>
2026-02-03 11:24:06 +1100

    plugin feature done

---
+++ OrangeSuper/App.tsx
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+  Text,
+} from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import React, { Suspense } from 'react';
+
+const TransactionsList = React.lazy(
+  () => import('TransactionsModule/TransactionsList'),
+);
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <AppContent />
+    </SafeAreaProvider>
+  );
+}
+
+function AppContent() {
+  return (
+    <View style={styles.container}>
+      <Suspense fallback={<Text>Loading...</Text>}>
+        <SafeAreaView style={styles.safeArea}>
+          <TransactionsList />
+        </SafeAreaView>
+      </Suspense>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'orange',
+  },
+  safeArea: {
+    flex: 1,
+  },
+});
+
+export default App;

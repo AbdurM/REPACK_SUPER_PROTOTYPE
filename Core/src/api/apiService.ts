4f1c7ad6e7827b3aeca8c29d7b3c286a247df229
Paras Korat <paras.korat@sscinc.com>
2026-03-10 18:46:13 +1100

    FIX: added libraries and URI

--- Core/src/api/apiService.ts
+++ Core/src/api/apiService.ts
@@ -3,6 +3,6 @@ import { RavenConfigResponse } from "../types/config.types";
 import { ravenClient } from "./clients/ravenClient"
 
 export const fetchRemoteConfig = async (): Promise<RavenConfigResponse[]> => {
-  const response = await ravenClient.get<RavenConfigResponse[]>("/api/configprop/raven-portal/dev/cg?name=raven-mobile-poc-config");
+  const response = await ravenClient.get<RavenConfigResponse[]>("/api/configprop/raven-portal/dev/cg?name=raven-portal-mobile-poc-config");
   return response.data;
 };
